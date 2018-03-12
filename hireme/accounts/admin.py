530a4d15bcbfb3335c2829074882c54c071010a5
Bhargav Ram <bhargavramkilambi@gmail.com>
2018-03-12 21:51:28 +0530

    Add files via upload

--- hireme/accounts/admin.py
+++ hireme/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category,Profile
+from .models import Category,UserProfile
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Profile)
+admin.site.register(UserProfile)
